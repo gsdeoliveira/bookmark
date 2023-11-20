6556f749d1ae5382155bb41ecf658553a41ff819
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-11-20 08:24:55 -0300

    menu mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,15 @@
 import logo from '../assets/images/logo-bookmark.svg'
-import burger from '../assets/images/icon-hamburger.svg'
-import Button from './Button'
+import Menu from './Menu'
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center py-5 px-5 md:py-10 sm:px-10 lg:px-28 text-sm md:text-base">
+    <nav className="flex justify-between items-center py-5 px-5 md:py-10 sm:px-10 lg:px-28">
       <div>
         <a href="#">
           <img src={logo} alt="bookmark" />
         </a>
       </div>
-      <div>
-        <ul className="hidden md:flex items-center gap-5 lg:gap-10 uppercase tracking-widest">
-          <li>
-            <a href="#" className="hover:text-softRed transition-colors">
-              Features
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-softRed transition-colors">
-              Pricing
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-softRed transition-colors">
-              Contact
-            </a>
-          </li>
-          <li>
-            <Button className="uppercase border-2 bg-softRed tracking-widest hover:bg-white hover:text-softRed hover:border-softRed transition-all">
-              Login
-            </Button>
-          </li>
-        </ul>
-        <img src={burger} alt="Menu" className="md:hidden" />
-      </div>
+      <Menu />
     </nav>
   )
 }
