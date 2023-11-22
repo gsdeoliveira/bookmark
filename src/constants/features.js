d14142527bddbe1f7affa6e6d14b9b0798ad5a64
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-11-22 01:15:09 -0300

    upload images to cloudinary

--- src/constants/features.js
+++ src/constants/features.js
@@ -1,21 +1,24 @@
 export const features = [
   {
     category: 'Simple Bookmarking',
-    image: '/src/assets/images/illustration-features-tab-1.svg',
+    image:
+      'https://res.cloudinary.com/dxrvtimah/image/upload/v1700626345/illustration-features-tab-1_gpbtiv.svg',
     title: 'Bookmark in one click',
     description:
       'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
   },
   {
     category: 'Speedy Searching',
-    image: '/src/assets/images/illustration-features-tab-2.svg',
+    image:
+      'https://res.cloudinary.com/dxrvtimah/image/upload/v1700626345/illustration-features-tab-2_rzg4ea.svg',
     title: 'Intelligent Search',
     description:
       'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
   },
   {
     category: 'Easy Sharing',
-    image: '/src/assets/images/illustration-features-tab-3.svg',
+    image:
+      'https://res.cloudinary.com/dxrvtimah/image/upload/v1700626345/illustration-features-tab-3_j840rx.svg',
     title: 'Share Your Bookmarks',
     description:
       'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
